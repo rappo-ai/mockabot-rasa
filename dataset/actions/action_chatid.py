2cefe18ac1d8547e74d4f37d0f3d17a30617578b
Renzil DSouza <renzil@gmail.com>
2021-06-04 16:30:33 +0530

    get_json_key to utils.json

--- dataset/actions/action_chatid.py
+++ dataset/actions/action_chatid.py
@@ -12,17 +12,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-def get_json_key(dict, key):
-    try:
-        key_split = key.split(".", 1)
-        key_split_len = len(key_split)
-        if key_split_len == 2:
-            return get_json_key(dict[key_split[0]], key_split[1])
-        elif key_split_len == 1:
-            return dict[key_split[0]]
-    except:
-        return None
-    return None
+from utils.json import get_json_key
 
 class ActionChatID(Action):
 
